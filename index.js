a62e8903c5f8ba8b8a9f9e33cfdacf016b7184e9
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-06-16 14:24:51 +0200

    added LocalStorage

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const toDoInput = document.querySelector("input");
-
 //                   FUNCTIONS
 
 //a function for the sort button to rearrange them alphabetically
@@ -9,6 +8,7 @@ function sort() {
   Array.from(ul.getElementsByTagName("LI"))
     .sort((a, b) => a.textContent.localeCompare(b.textContent))
     .forEach((li) => ul.append(li));
+  localStore();
 }
 
 //a function to change the gif from waiting to writing and added writing sounds
@@ -30,6 +30,73 @@ function changeImage() {
   }, 1000);
 }
 
+//store inputs into localstorage
+function localStore() {
+  let list = document.querySelectorAll("li");
+  let storedList = [];
+  list.forEach((li) => {
+    storedList.push({
+      text: li.firstChild.textContent,
+    });
+  });
+  localStorage.setItem("isTask", JSON.stringify(storedList));
+}
+
+function getItem() {
+  const storedItem = JSON.parse(localStorage.getItem("isTask"));
+  document.querySelectorAll("li").forEach((li) => li.remove());
+  if (storedItem) {
+    storedItem.forEach((task) => {
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      const button = document.createElement("button");
+      const edit = document.createElement("button");
+      const editfield = document.createElement("input");
+      const button2 = document.createElement("button");
+      p.textContent = task.text;
+      button.textContent = "Remove";
+      button.classList.add("delete-btn");
+      edit.textContent = "Edit";
+      edit.classList.add("edit-btn");
+      editfield.classList.add("editfield");
+      editfield.placeholder = "Edit";
+      button2.textContent = "Submit";
+      button2.classList.add("submit2");
+
+      button.addEventListener("click", () => {
+        li.remove();
+        localStore();
+      });
+
+      edit.addEventListener("click", () => {
+        function editor() {
+          if (editfield.value.match(/^ *$/)) return;
+          li.append(p, edit, button);
+          p.textContent = editfield.value;
+          editfield.remove();
+          button2.remove();
+          changeImage();
+          localStore();
+        }
+        editfield.value = p.textContent;
+        p.remove();
+        edit.remove();
+        button.remove();
+        li.append(editfield, button2);
+        editfield.focus();
+        button2.addEventListener("click", () => {
+          editor();
+        });
+        editfield.addEventListener("keyup", (e) => {
+          if (e.code !== "Enter") return;
+          editor();
+        });
+      });
+      li.append(p, edit, button);
+      document.querySelector("ul").append(li);
+    });
+  }
+}
 //The main submit function, what happens when you press enter or hit the submit button
 function submit() {
   if (!toDoInput.value || toDoInput.value.match(/^ *$/)) return;
@@ -51,7 +118,10 @@ function submit() {
   button2.textContent = "Submit";
   button2.classList.add("submit2");
 
-  button.addEventListener("click", () => li.remove());
+  button.addEventListener("click", () => {
+    li.remove();
+    localStore();
+  });
 
   edit.addEventListener("click", () => {
     function editor() {
@@ -81,6 +151,8 @@ function submit() {
   document.querySelector("ul").append(li);
   toDoInput.value = "";
   toDoInput.focus();
+  localStore();
+  console.log(localStorage);
 }
 
 //code for the Enter key to work with the submit form
@@ -92,4 +164,6 @@ toDoInput.addEventListener("keyup", (e) => {
 // the command for the Clear List button, deletes every element that was added
 document.querySelector("#deleteAll").addEventListener("click", () => {
   document.querySelectorAll("li").forEach((li) => li.remove());
+  localStorage.clear();
 });
+getItem();
